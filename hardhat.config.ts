a50dbdb1aeaa8568adce3895139e57d785c27b25
Jack Hamer <jackhamer0990@gmail.com>
2023-10-24 19:53:34 +0300

    fix: downgrade verify plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 
+import "@matterlabs/hardhat-zksync-node";
 import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-solc";
 import "@matterlabs/hardhat-zksync-verify";
