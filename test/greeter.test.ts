9df18762c160c5a7d95339e68c45b6cd36165b67
Jack Hamer <jackhamer0990@gmail.com>
2023-10-25 13:49:38 +0300

    fix: change greeter test file name

--- test/greeter.test.ts
+++ test/greeter.test.ts
@@ -21,6 +21,7 @@ describe('Greeter', function () {
     expect(await greeter.greet()).to.eq('Hi');
 
     const setGreetingTx = await greeter.setGreeting('Hola, mundo!');
+    
     // wait until the transaction is mined
     await setGreetingTx.wait();
 
